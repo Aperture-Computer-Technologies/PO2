8e49971703d69ae60e93e9d4f14346bbb496c714
Sagar Ramsaransing <MassiveAtoms@localhost.localdomain>
2021-01-09 19:19:54 -0300

    fixed jupyter

--- test/hash-test.cpp
+++ test/hash-test.cpp
@@ -1,8 +1,7 @@
 #include "./hash.h"
+
 #include <iostream>
+
 #include "./hash-test.h"
 
-int main() {
-    hash_test_suite(example, "naive_modulo");
-
-}
+int main() { hash_test_suite(example, "naive_modulo"); }
