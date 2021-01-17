85714df43530f09b200e70fed067013aac6888f0
Sagar Ramsaransing <MassiveAtoms@localhost.localdomain>
2021-01-17 20:22:53 -0300

    fixing some things

--- test/hash.h
+++ test/hash.h
@@ -15,9 +15,27 @@ size_t dumb_bitmask(int key, int size)
     else {
         return ans;
     }
-}// Use folding on a string, summed 4 bytes at a time
-int sfold(int s, int M) {
- 
- return (s * 256) % M;
+}  // Use folding on a string, summed 4 bytes at a time
+int sfold(int s, int M) { return (s * 256) % M; }
+
+unsigned int PJWHash(const char* str, unsigned int length)
+{
+    const unsigned int BitsInUnsignedInt = (unsigned int)(sizeof(unsigned int) * 8);
+    const unsigned int ThreeQuarters = BitsInUnsignedInt * 0.75;
+    const unsigned int OneEighth = BitsInUnsignedInt / 8;
+    const unsigned int HighBits = (unsigned int)(0xFFFFFFFF) << (BitsInUnsignedInt - OneEighth);
+    unsigned int hash = 0;
+    unsigned int test = 0;
+    unsigned int i = 0;
+
+    for (i = 0; i < length; ++str, ++i) {
+        hash = (hash << OneEighth) + (*str);
+
+        if ((test = hash & HighBits) != 0) {
+            hash = ((hash ^ (test >> ThreeQuarters)) & (~HighBits));
+        }
+    }
+
+    return hash;
 }
 #endif
